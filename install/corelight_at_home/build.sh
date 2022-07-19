7fb088f10db8658e53e540260cf287fd541e808a
Colin Stubbs <colin.stubbs@equatetechnologies.com.au>
2022-07-19 12:11:44 +1000

    Update, basic working build

--- install/corelight_at_home/build.sh
+++ install/corelight_at_home/build.sh
@@ -1,9 +1,5 @@
-CONTAINER_ORG=colin-stubbs
-CONTAINER_NAME=corelight
-CONTAINER_TAG=latest
-
 # remove any old containers
-docker rmi --force "${CONTAINER_ORG}/${CONTAINER_NAME}"
+docker compose -f ./docker-compose.yml rm
 
 # build the container
-docker build --rm=true --force-rm=true --no-cache -t "${CONTAINER_ORG}/${CONTAINER_NAME}:${CONTAINER_TAG}" -f container/Dockerfile container
+docker compose -f ./docker-compose.yml build
